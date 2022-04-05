71a318992b952212a72795d321b1278b3dd10ea9
B3r4ti0n <arcoucou77@gmail.com>
2022-04-05 13:02:42 +0200

    first commit Alexis R.

---
+++ Projet-ios-2022/Projet-ios-2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projet-ios-2022
+//
+//  Created by Alexis Rabiller on 05/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
