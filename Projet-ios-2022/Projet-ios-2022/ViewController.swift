d4f1acf5ba6d64a9f28eadf94fdf6a5f1441bbcd
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2022-04-06 11:30:34 +0200

    aa

--- Projet-ios-2022/Projet-ios-2022/ViewController.swift
+++ Projet-ios-2022/Projet-ios-2022/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Home"
         // Do any additional setup after loading the view.
     }
 
