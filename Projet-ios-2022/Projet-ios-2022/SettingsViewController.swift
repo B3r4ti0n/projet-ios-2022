f91ecb15af0f82cc16eb5e5dfaf44297181ccf79
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2022-04-06 19:34:45 +0200

    ajout design settings; ajout highscore | Alexis-Pel

--- Projet-ios-2022/Projet-ios-2022/SettingsViewController.swift
+++ Projet-ios-2022/Projet-ios-2022/SettingsViewController.swift
@@ -7,45 +7,68 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-    var fileURL:URL? = nil
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        var settingsJson: [String: AnyObject]
-        
-        let file = "settings.txt" //this is the file. we will write to and read from it
-        //let text = "{\"username\":\"Spriingo\",\"highTime\":1102,\"difficulty\":0}" //just a text
-        
+class Setting{
+    var fileURL:URL?
+    let file = "settings.txt" //this is the file. we will write to and read from it
+    var settingsJson: [String: AnyObject]?
+    
+    init(){
         if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            fileURL = dir.appendingPathComponent(file)
+            self.fileURL = dir.appendingPathComponent(file)
+            print(self.fileURL)
+            self.settingsJson = self.readSave()
             //writeSave(text: text)
-            settingsJson = readSave()
         }
-
-        // Do any additional setup after loading the view.
     }
     
-    func readSave() -> [String:AnyObject]{
+    func readSave() -> [String:AnyObject]?{
         do {
             let text = try String(contentsOf: fileURL!, encoding: .utf8)
             let data = Data(text.utf8)
             // make sure this JSON is in the format we expect
             if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] {
-                print("Find JSON")
                 return json
             }
         }
         catch {/* error handling here */}
-        let error: [String:AnyObject] = [:]
-        return error
+        return nil
     }
     
-    func writeSave(text: String){
+    func writeSave(username: String, highScore: Int, difficulty: Int){
+        let line = "{\"username\":\"\(username)\",\"highTime\":\(highScore),\"difficulty\":\(difficulty)}"
         do {
-            try text.write(to: fileURL!, atomically: false, encoding: .utf8)
+            try line.write(to: self.fileURL!, atomically: false, encoding: .utf8)
         }
         catch {/* error handling here */}
+    }
+}
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    let settings = Setting()
+    var settingsJson: [String:AnyObject] = [:]
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingsJson = settings.settingsJson!
+        usernameTextField.text = settingsJson["username"] as? String
+        highScoreLabel.text = String((settingsJson["highTime"] as? Int)!)
+    
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func saveSettings(_ sender: Any) {
+        settings.writeSave(username: usernameTextField.text!, highScore: self.settingsJson["highTime"] as! Int, difficulty: 0)
+        self.loading.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.loading.stopAnimating()
+            self.saveButton.setTitle("Compte Modifié", for: .normal)
+            self.saveButton.backgroundColor = UIColor.green
+            self.saveButton.layer.opacity = 0.8
+        }
     }
     
 
